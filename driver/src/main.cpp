f928945d11741385dc39c50e349bf17b7e18f2c9
Adrian Schollmeyer <git@nexadn.de>
2025-03-06 22:32:31 +0100

    drv: enable optional landlock support
    This enables support for landlock using liblandlockpp, thus hardening
    the driver against security issues caused by software bugs. This is
    important as this driver might be called with root permissions to obtain
    access to hidraw.

--- driver/src/main.cpp
+++ driver/src/main.cpp
@@ -1,15 +1,26 @@
+#include <cerrno>
+#include <filesystem>
 #include <iostream>
 #include <memory>
 #include <sstream>
 
 #include <jack/types.h>
+#include <system_error>
 #include <thread>
+#include <utility>
 
+#include "config.h"
 #include "io/HidDevice.hpp"
 #include "io/JackWrapper.hpp"
 #include "io/MidiEvent.hpp"
 #include "tkf1/F1Device.hpp"
 #include "tkf1/IOMapper.hpp"
+
+#ifdef TKF1_HAVE_LANDLOCK
+# include <ll/ActionType.hpp>
+# include <ll/Rule.hpp>
+# include <ll/Ruleset.hpp>
+#endif
 
 using namespace std::chrono_literals;
 using Color = F1Device::ButtonColor;
@@ -44,10 +55,98 @@ constexpr Color YELLOW = F1Device::rgb2color(
 	F1Device::FULL_BRIGHTNESS, F1Device::FULL_BRIGHTNESS, F1Device::DARK
 );
 } // namespace colors
+
+#ifdef TKF1_HAVE_LANDLOCK
+void setup_landlock()
+{
+	landlock::Ruleset ll_ruleset(
+		{landlock::action::FS_EXECUTE,
+		 landlock::action::FS_WRITE_FILE,
+		 landlock::action::FS_READ_FILE,
+		 landlock::action::FS_READ_DIR,
+		 landlock::action::FS_REMOVE_DIR,
+		 landlock::action::FS_REMOVE_FILE,
+		 landlock::action::FS_MAKE_CHAR,
+		 landlock::action::FS_MAKE_DIR,
+		 landlock::action::FS_MAKE_SOCK,
+		 landlock::action::FS_MAKE_FIFO,
+		 landlock::action::FS_MAKE_BLOCK,
+		 landlock::action::FS_MAKE_SYM,
+		 landlock::action::FS_REFER,
+		 landlock::action::FS_TRUNCATE},
+		{}
+	);
+	// clang-format off
+
+	// access to hidraw and pipewire config
+	ll_ruleset.add_rule(std::move(
+		landlock::PathBeneathRule{}
+			.add_path(std::filesystem::absolute("/dev"))
+			.add_path("/usr/share")
+			.add_action(landlock::action::FS_READ_FILE)
+			.add_action(landlock::action::FS_WRITE_FILE)
+			.add_action(landlock::action::FS_READ_DIR)
+	));
+
+	// libjack permissions
+	landlock::PathBeneathRule ll_rule_libjack;
+	ll_rule_libjack
+		.add_action(landlock::action::FS_READ_DIR)
+		.add_action(landlock::action::FS_READ_FILE)
+		.add_action(landlock::action::FS_EXECUTE);
+	// clang-format on
+
+	for (const auto* path : {
+		     "/bin",
+		     "/lib",
+		     "/lib32",
+		     "/lib64",
+	     }) {
+		for (const auto* prefix : {"/", "/usr", "/usr/local"}) {
+			try {
+				ll_rule_libjack.add_path(
+					std::filesystem::absolute(prefix) /
+					std::filesystem::absolute(path)
+				);
+			} catch (std::system_error& e) {
+				// Ignore directories that don't exist
+				if (e.code() ==
+				    std::error_code{
+					    ENOENT, std::system_category()
+				    }) {
+					continue;
+				}
+
+				throw e;
+			}
+		}
+	}
+	ll_ruleset.add_rule(std::move(ll_rule_libjack));
+
+# if __has_feature(address_sanitizer) || defined(__SANITIZE_ADDRESS__)
+	// clang-format off
+	ll_ruleset.add_rule(std::move(
+		landlock::PathBeneathRule{}
+			.add_path(std::filesystem::absolute("/proc"))
+			.add_action(landlock::action::FS_READ_DIR)
+			.add_action(landlock::action::FS_READ_FILE)
+	));
+	// clang-format on
+# endif
+
+	// enforce() deferred to after JACK has started so we don't disturb
+	// JACK's operation
+	ll_ruleset.enforce();
+}
+#endif
 } // namespace
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
 {
+#ifdef TKF1_HAVE_LANDLOCK
+	setup_landlock();
+#endif
+
 	std::shared_ptr<F1Device> dev = discover_device();
 	std::shared_ptr<JackWrapper> jack;
 	IOMapper io_mapper;
